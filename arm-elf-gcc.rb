e7235bf424f70a8ae026c5e98ed4d092af19976b
Alex Parker <3zanders@gmail.com>
2017-10-18 20:47:12 +0100

    Update arm-elf-gcc.rb

--- arm-elf-gcc.rb
+++ arm-elf-gcc.rb
@@ -4,7 +4,7 @@ class ArmElfGcc < Formula
   homepage 'http://gcc.gnu.org'
   url "http://mirror.tochlab.net/pub/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.bz2"
   mirror "https://ftp.gnu.org/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.bz2"
-  sha256 "8a8136c235f64c6fef69cac0d73a46a1a09bb250776a050aec8f9fc880bebc17f"
+  sha256 "8a8136c235f64c6fef69cac0d73a46a1a09bb250776a050aec8f9fc880bebc17"
 
   depends_on 'gmp'
   depends_on 'libmpc'
