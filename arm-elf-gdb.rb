1889afd6049b705f857b47af2b65113e7d091a5c
tom <tomcutler39@gmail.com>
2020-02-09 15:05:46 +0000

    gdb-7.11.1 -> gdb-9.1

--- arm-elf-gdb.rb
+++ arm-elf-gdb.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class ArmElfGdb < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.11.1.tar.xz'
-  sha256 'e9216da4e3755e9f414c1aa0026b626251dfc57ffe572a266e98da4f6988fc70'
+  url 'https://ftp.gnu.org/gnu/gdb/gdb-9.1.tar.gz'
+  sha256 'fcda54d4f35bc53fb24b50009a71ca98410d71ff2620942e3c829a7f5d614252'
 
   depends_on 'arm-elf-binutils'
   depends_on 'arm-elf-gcc'
 
   def install
-    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-7'
-    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-7'
-    ENV['CPP'] = '/usr/local/opt/gcc/bin/cpp-7'
-    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-7'
+    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-9'
+    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-9'
+    ENV['CPP'] = '/usr/local/opt/gcc/bin/cpp-9'
+    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-9'
 
     mkdir 'build' do
       system '../configure', '--target=arm-elf-eabi', "--prefix=#{prefix}" ,'--disable-werror'
